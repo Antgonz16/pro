4a279ac255340a169252aa62496ac56fe01cd696
javroi <javroi@alu.iesserpis.org>
2019-10-07 13:25:14 +0200

    Boletin 1 actualizado

--- boletines/src/boletin1/ejer8.java
+++ boletines/src/boletin1/ejer8.java
@@ -2,4 +2,17 @@ package src.boletin1;
 
 public class ejer8 {
 
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		System.out.println("Bienvenido a mi calculadora");
+		System.out.println("El valor de a es : " + a);
+		System.out.println("El valor de a es : " + b);
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % b = " + (a % b));
+	}
+
 }
