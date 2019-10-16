adb83ddce916f5637b4e58df27aa13e10ae04fb2
javroi <javroi@alu.iesserpis.org>
2019-10-16 09:44:13 +0200

    actualizacion

--- boletines/src/boletin3_1/ejer5.java
+++ boletines/src/boletin3_1/ejer5.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class ejer5 {
 
 	public static void main(String[] args) {
+		//Calcula la nota necesaria para la media que quieres
 		Scanner scanner = new Scanner(System.in);
 		double nota1 = 0.0;
 		double nota2 = 0.0;
 		double media = 0.0;
 		
+		//Lectura de datos
 		System.out.println("Introduce la nota del primer examen: ");
 		nota1 = scanner.nextDouble();
 		System.out.println("Que nota quieres sacar en el trimestre?");
 		media = scanner.nextDouble();
 		
+		//Calculo de los datos
 		nota2 = (media - (nota1*0.4))/0.6;
 		nota2 = Math.rint(nota2*100)/100;
 		
-		
+		//Escritura de datos
 		System.out.println("Para tener un "+media+" en el trimestre necesitas sacar un "+nota2+"en el segundo examen");
 	}
 }
--- boletines/src/boletin3_1/ejer7.java
+++ boletines/src/boletin3_1/ejer7.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class ejer7 {
 
 	public static void main(String[] args) {
+		//Pasa de km/h a m/s
 		Scanner scanner = new Scanner(System.in);
 		double velocidad_km = 0.0;
 		double velocidad_m = 0.0;
 		
+		//Lectura de datos
 		System.out.println("Dame una velocidad en km/h: ");
+		
+		//Calculo de datos
 		velocidad_km = scanner.nextDouble();
 		velocidad_m = (velocidad_km*1000)/360;
+		
+		//Escritura de datos
 		System.out.println("La velocidad em m/s: " + velocidad_m);
 
 	}
