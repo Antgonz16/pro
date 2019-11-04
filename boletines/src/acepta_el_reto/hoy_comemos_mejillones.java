1c3a129408db94f842aa3a79a32489d7879f74c9
javroi <javroi@alu.iesserpis.org>
2019-11-04 18:57:37 +0100

    dos programas acepta el reto

--- boletines/src/acepta_el_reto/hoy_comemos_mejillones.java
+++ boletines/src/acepta_el_reto/hoy_comemos_mejillones.java
@@ -3,27 +3,32 @@ package src.acepta_el_reto;
 import java.util.Scanner;
 
 public class hoy_comemos_mejillones {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n_mejillones,medida;
-        int contador=0;
+        int n_mejillones, medida, medio, fin, ini, contador;
         int[] montones;
-        
 
-        while(scanner.hasNext()){
+        while (scanner.hasNext()) {
             n_mejillones = scanner.nextInt();
-            montones= new int[n_mejillones];
-            contador=1;
-            for(int i=0; i<n_mejillones; i++){
+            montones = new int[n_mejillones];
+            montones[0] = scanner.nextInt();
+            contador = 1;
+            for (int i = 1; i < n_mejillones; i++) {
                 medida = scanner.nextInt();
-                montones[i]=medida;
-            }
-            for(int i=0; i<n_mejillones; i++){
-                for(int j=0; j<i; j++){
-                    if(i>0&&montones[i]<montones[j]){
-                        contador++;
-                        j=i;
+                if (medida >= montones[contador - 1]) {
+                    montones[contador++] = medida;
+                } else {
+                    fin = contador;
+                    ini = 0;
+                    while (ini < fin) {
+                        medio = (fin + ini) / 2;
+                        if (montones[medio] > medida) {
+                            fin = medio;
+                        } else {
+                            ini = medio + 1;
+                        }
                     }
+                    montones[fin] = medida;
                 }
             }
 
