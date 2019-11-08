acb9e01e6cb4cf32b30eb9e2e73b8f053e6a0eec
javroi <javroi@alu.iesserpis.org>
2019-11-08 13:46:06 +0100

    .

--- acepta_el_reto/acepta_el_reto/aracnidos.java
+++ acepta_el_reto/acepta_el_reto/aracnidos.java
@@ -2,29 +2,23 @@ package acepta_el_reto;
 
 import java.util.*;
 
-import javax.smartcardio.ATR;
-
 public class aracnidos {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int artropodo,anillos;
+		int insectos,aracnidos,crustraceos,escalopendra,anillos;
 		int caso;
 		int patas;
 		
 		caso = scanner.nextInt();
 		
 		for (int i = 0; i < caso; i++) {
-			artropodo=scanner.nextInt();
-			patas=artropodo*6;
-			artropodo=scanner.nextInt();
-			patas=patas+artropodo*8;
-			artropodo=scanner.nextInt();
-			patas=patas+artropodo*10;
-			artropodo=scanner.nextInt();
+			insectos=scanner.nextInt();
+			aracnidos=scanner.nextInt();
+			crustraceos=scanner.nextInt();
+			escalopendra=scanner.nextInt();
 			anillos=scanner.nextInt();
-			patas=patas+artropodo*anillos*2;
-			
+			patas=insectos*6+aracnidos*8+crustraceos*10+escalopendra*anillos*2;
 			System.out.println(patas);
 		}
 		
